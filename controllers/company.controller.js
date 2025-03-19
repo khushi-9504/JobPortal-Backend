4d24f0134d55f254e3b4fca82df54d7cd48183ff
khushi-9504 <khushaligajera028wdt@gmail.com>
2025-03-19 13:48:50 +0530

    Update company.controller.js

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -73,8 +73,11 @@ export const updateCompany = async (req, res) => {
   try {
     const { name, description, website, location } = req.body;
     const file = req.file;
+    const fileUri = getDataUri(file);
+    const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+    const logo = cloudResponse.secure_url;
 
-    const updateData = { name, description, website, location };
+    const updateData = { name, description, website, location,logo };
 
     const company = await Company.findByIdAndUpdate(req.params.id, updateData, {
       new: true,
