0711403feb285cce16fb46dd07c5b2ea4b3201f1
Tapasvi Chhabhaiya <117344055+TTHPATEL@users.noreply.github.com>
2025-03-21 14:12:01 +0530

    DELETE JOB - ADMIN
    DELETE JOB - ADMIN

--- routes/job.route.js
+++ routes/job.route.js
@@ -1,12 +1,19 @@
 import express from "express";
 import authenticateToken from "../middleware/isAuthenticated.js";
-import { getAdminJob, getAllJobs, getJobById, postJob } from "../controllers/job.controller.js";
+import {
+  getAdminJob,
+  getAllJobs,
+  getJobById,
+  postJob,
+  deleteJob,
+} from "../controllers/job.controller.js";
 
 const router = express.Router();
 
-router.route("/post").post(authenticateToken,postJob);
-router.route("/get/:id").get(authenticateToken,getJobById);
-router.route("/getadminjobs").get(authenticateToken,getAdminJob);
-router.route("/get").get(authenticateToken,getAllJobs);
+router.route("/post").post(authenticateToken, postJob);
+router.route("/get/:id").get(authenticateToken, getJobById);
+router.route("/getadminjobs").get(authenticateToken, getAdminJob);
+router.route("/get").get(authenticateToken, getAllJobs);
+router.route("/delete/:id").delete(authenticateToken, deleteJob);
 
 export default router;
