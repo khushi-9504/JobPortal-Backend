f7aa130258d7dde7adf222194cb09d31520be288
Tapasvi Chhabhaiya <117344055+TTHPATEL@users.noreply.github.com>
2025-03-21 16:56:02 +0530

    DYNAMIC ICON AT DESCRIPTION - STUDENT
    DYNAMIC ICON AT DESCRIPTION - STUDENT

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -87,9 +87,9 @@ export const getAllJobs = async (req, res) => {
 export const getJobById = async (req, res) => {
   try {
     const jobId = req.params.id;
-    const job = await Job.findById(jobId).populate({
-      path: "applications",
-    });
+    const job = await Job.findById(jobId)
+      .populate("company") // Populate company details
+      .populate("applications");
 
     if (!job) {
       return res.status(404).json({ message: "Job not found", status: false });
